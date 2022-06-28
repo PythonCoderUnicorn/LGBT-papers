1b3013573bc431ae425266bfb45e0baa3b77749a
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-06-28 10:37:27 -0600

    LGBT & preg

--- datasets/Trans & pregnancy/trans&preg.R
+++ datasets/Trans & pregnancy/trans&preg.R
@@ -68,6 +68,69 @@ genders %>%
 
 
 
+# ---------
+
+df %>% 
+  select(sample_characteristics, x4) %>% 
+  slice(40:50) %>% # sex orientation
+  transmute(sexual_orientation = factor(sample_characteristics),
+         count = as.numeric(x4)) %>% 
+  ggplot(
+    aes(x= count,
+        y= fct_reorder(sexual_orientation, count),
+        fill= count)
+  )+
+  geom_col()+
+  ggdark::dark_mode() +
+  scale_fill_paletteer_c(`"grDevices::rainbow"`)+
+  coord_cartesian(xlim = c(0, 150), expand = F)+
+  labs(
+    title = "\nLGBT people who ever had a pregnancy",
+    subtitle = "n = 210 in US",
+    x="\nnumber reported ever having had a pregnancy\n",
+    y="",
+    caption = "\n @StarTrek_Lt | June 28, 2022 | Data: Moseson et al, 2020, IJTH DOI:10.1080/26895269.2020.1841058"
+  )+
+  theme(
+    text = element_text(family = 'Lato'),
+    plot.caption = element_textbox(face = 'italic', 
+                                   color='grey70',
+                                   hjust = 1,
+                                   size = 11
+    ),
+    plot.title = element_textbox(size = 15, hjust = 0.5, face = 'bold'),
+    plot.subtitle = element_text(hjust = 0.5, size=13),
+    axis.text.y = element_text(color = 'white', size=12),
+    axis.text.x = element_text(color = 'white'),
+    plot.margin = margin(t=1,r=1,b=1, 0, unit = 'cm')
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
